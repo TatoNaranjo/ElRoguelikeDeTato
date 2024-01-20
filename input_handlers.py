7ab88544585821903c77842a25df008b8584ab44
SANTIAGO NARANJO HERRERA <snaranjoh@ucundinamarca.edu.co>
2024-01-19 22:16:18 -0500

    First files & moving the character

---
+++ input_handlers.py
@@ -0,0 +1,51 @@
+#Python's type hinting system
+from typing import Optional
+
+#Importing tcod event system to use tcod's event system.
+import tcod.event
+#Importing the Action class and it's subclasses from actions.
+from actions import Action,EscapeAction,MovementAction
+
+#Creating a class called EventHandler which is a subclass of tcod's EventDispatch class.
+#Event Dispatch allows us to send an event to his proper method.
+class EventHandler(tcod.event.EventDispatch[Action]):
+
+    #ev_quit is called when we receive a "quit" event, it happens when the player click 'X'
+    #on the window of the program.
+    def ev_quit(self,event: tcod.event.Quit)-> Optional[Action]:
+        
+        #Quitting the program with SystemExit()
+        raise SystemExit()
+    
+    #Method that will receive key press events and return either an Action subclass or None
+    #if no valid key was pressed.
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+
+        #action is the variable that will hold whatever subclass of Action we end up assigning it
+        #to. If no valid key press is found it remains set to None.
+        action:Optional[Action]=None
+
+        #Key holds the actual key the player pressed. Without additional info about modifiers like
+        #'Shift' or 'Alt'
+        key = event.sym
+
+        #If the up key is pressed, our character moves upwards.
+        if key == tcod.event.K_UP:
+            action = MovementAction(dx=0,dy = -1)
+        #If the up key is pressed, our character moves backwards.
+        elif key == tcod.event.K_DOWN:
+            action = MovementAction(dx=0,dy=1)
+        #If the up key is pressed, our character moves leftwards.
+        elif key == tcod.event.K_LEFT:
+            action = MovementAction(dx=-1,dy=0)
+        #If the up key is pressed, our character moves rightwards.
+        elif key == tcod.event.K_RIGHT:
+            action = MovementAction(dx=1,dy = 0)
+
+        #If the player presed the 'Escape' key, we return EscapeAction to exit the game.
+        #EscapeAction will be used to do things like exit menus in a future.
+        elif key == tcod.event.K_ESCAPE:
+            action = EscapeAction()
+        
+        #We return the action.
+        return action
---
+++ main.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+import tcod
+
+#Importing the EscapeAction and MovementAction from actions and EventHandler from input_handlers
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+
+def main() ->None:
+    #Defining Variables for the Screen Size. 
+    #Todo: load this values from a JSON File.
+    screen_width = 80
+    screen_height = 50
+
+    #Tracking player's position at all times.
+    player_x = int (screen_width/2);
+    player_y = int (screen_height/2);
+
+    #tcod will use our font from dejavu10x10_gs_tc.png
+    tileset = tcod.tileset.load_tilesheet(
+        'dejavu10x10_gs_tc.png',32,8,tcod.tileset.CHARMAP_TCOD
+    )
+
+    #An instance of our EventHandler Class
+    event_handler = EventHandler()
+    #This creates the Screen
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset = tileset,
+        title = "El Roguelike de Tato",
+        vsync=True,
+    ) as context:
+        #Creates the console to display the elements
+
+        #By default numpy acceses 2D arrays in [y,x] so this line reverses it to [x,y]
+        root_console = tcod.console.Console(screen_width,screen_height,order = "F")
+
+        #Game loop core init.
+        while True:
+            #Printing our @ character.
+            root_console.print(x=player_x,y=player_y, string = '@')
+
+            #This line is the core of the screen updating.
+            #context.present updates the screen whit what we've told to display so far.
+            context.present(root_console)
+
+            root_console.clear();
+            
+            #If we hit the X button in the console's window, the game will exit.
+            #That's our secure exit logic.
+            for event in tcod.event.wait():
+                
+                #Sending the event to our event_handler's dispatch method, to send the
+                #Event to its proper place.
+                action = event_handler.dispatch(event)
+
+                #If no valid key was pressed just skip the loop
+                if action is None:
+                    continue
+                
+                """
+                if the action is an instance of the class MovementAction, we move our "@" character.
+                We grab the dx and dy values to gave to MovementAction earlier,
+                which will move our '@' character in which direction we want it to move.
+
+                Regardless of what the value is, we add dx and dy to player_x and player_y respectively.
+                Because the console is using player_x and player_y to draw where our '@' character is,
+                modify these two values will cause the symbol to move.
+                """
+                if(isinstance(action,MovementAction)):
+                    player_x+=action.dx
+                    player_y+=action.dy
+    
+                #Else we quit the program if the player hits 'Esc' key.
+                elif isinstance(action,EscapeAction):
+                    raise SystemExit()
+
+if __name__ == "__main__":
+    main()
